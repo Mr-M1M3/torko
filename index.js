5e233df3b40918bb1a959cf0013c62fd93a01266
Mr M1M3 <abir.sheikh.4044@gmail.com>
2022-04-15 15:05:58 +0600

    version 3

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-/**
- * Project: torko
- * Author: Mr-M1M3
- * Date: 14-04-2022
- */
-
-// requirements
-const Flags = require('./lib/flags');
-const Args = require('./lib/args');
-
+const Torko = require('./lib/parser');
 const process = require('process');
-
-function torko() {
-    return {
-        flags: new Flags(process.argv.slice(2)),
-        arguments: new Args(process.argv.slice(2))
-    }
+function parser(argv = process.argv.slice(2)){
+    return new Torko(argv);
 }
 
-module.exports = torko;
+module.exports = parser;
